44a423a1f31af5e3d03e291dce38e01172d7aa43
Alexander Wright <Alexwright45177@gmail.com>
2025-02-24 18:32:03 -0500

    Update FortuneTellerViewer.java

--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class FortuneTellerViewer {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            FortuneTellerFrame frame = new FortuneTellerFrame();
+            frame.setVisible(true);
+        });
+    }
 }
